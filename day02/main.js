d0224cae10fcacc099acb5ed26ffe4675e49687c
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-04-10 04:45:30 +0500

    Commited Day 02

---
+++ day02/main.js
@@ -0,0 +1,11 @@
+//Question # 01
+console.log("Martin Luther King,Jr. said \"It is always the right time to do the right thing.\"");
+//Question # 02
+var famous_person = "Martin Luther King,Jr.";
+var message = "said \"It is always the right time to do the right thing.\"";
+console.log(famous_person, message);
+//Question # 03
+var WhiteSpace = "\n\tSyed Huzaifa Ali\t\n";
+console.log("With Whitespaces : ", WhiteSpace);
+var WithoutSpace = WhiteSpace.trim();
+console.log("Without Whitespaces : ", WithoutSpace);
---
+++ day02/main.ts
@@ -0,0 +1,13 @@
+//Question # 01
+console.log("Martin Luther King,Jr. said \"It is always the right time to do the right thing.\"");
+
+//Question # 02
+let famous_person = "Martin Luther King,Jr.";
+let message = "said \"It is always the right time to do the right thing.\"";
+console.log(famous_person,message);
+
+//Question # 03
+let WhiteSpace = "\n\tSyed Huzaifa Ali\t\n";
+console.log("With Whitespaces : ",WhiteSpace);
+let WithoutSpace = WhiteSpace.trim();
+console.log("Without Whitespaces : ",WithoutSpace);
