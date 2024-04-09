7dd5fd83113f806f1f84427fe15aa5b806e702d9
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-04-10 03:20:21 +0500

    Commited Day 01

---
+++ day01/main.ts
@@ -0,0 +1,11 @@
+//Question # 01
+/*Installed Node JS and Typescript and VS Code */
+
+//Question # 02
+var pname = "Huzaifa Ali";
+console.log("Hello " + pname + " would you like to learn some Python today?");
+
+//Question # 03
+console.log("Lowercase : ",pname.toLowerCase());
+console.log("Uppercase : ",pname.toUpperCase());
+console.log("Titlecase : ",pname.replace(/\b\w/g, c => c.toUpperCase()));
