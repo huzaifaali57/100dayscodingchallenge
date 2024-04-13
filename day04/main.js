7b7726da59f723fb5401e8bf031d9f56ce5167a6
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-04-13 16:09:09 +0500

    Commited Day 04

---
+++ day04/main.js
@@ -0,0 +1,21 @@
+// Question 10
+// Syed Huzaifa Ali        28/02/2024
+/*The program below is a four line code in which four operation
+i.e. addition,subtraction,multiplication and division are being performed
+and each line will print 8 as the output so there will be four 8's in the answer */
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+// Question 11
+var friends = ["Shahbaz", "Hanzala", "Haider", "Yaseen", "Murtaza", "Asher", "Khizar"];
+var i = 0;
+for (i = 0; i <= 6; i++) {
+    console.log(friends[i]);
+}
+//friends.forEach(friends => console.log(friends));
+// Question 12
+//Using the same array from Question 11
+for (var i_1 = 0; i_1 <= 6; i_1++) {
+    console.log("What's Up", friends[i_1], "How are you?");
+}
---
+++ day04/main.ts
@@ -0,0 +1,25 @@
+// Question 10
+// Syed Huzaifa Ali        28/02/2024
+/*The program below is a four line code in which four operation
+i.e. addition,subtraction,multiplication and division are being performed
+and each line will print 8 as the output so there will be four 8's in the answer */
+console.log(5+3);
+console.log(10-2);
+console.log(2*4);
+console.log(16/2);
+
+// Question 11
+let friends = ["Shahbaz","Hanzala","Haider","Yaseen","Murtaza","Asher","Khizar"];
+let i = 0;
+for(i=0;i<=6;i++)
+{
+    console.log(friends[i]);
+}
+//friends.forEach(friends => console.log(friends));
+
+// Question 12
+//Using the same array from Question 11
+for(let i=0;i<=6;i++)
+{
+    console.log("What's Up",friends[i], "How are you?");
+}
