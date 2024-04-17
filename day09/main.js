ea5807693bb459a7d7fadac4496ca538d2a95da2
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-04-18 02:57:51 +0500

    Commited Day 09

---
+++ day09/main.js
@@ -0,0 +1,62 @@
+// Question 25
+console.log("\n\tQuestion 25 Starts Here\t\n");
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("Player Earned 5 Points");
+}
+else {
+    console.log("No Output");
+}
+// Question 26
+console.log("\n\tQuestion 26 Starts Here\t\n");
+// This Version runs the If Block
+var alien_1_color = "green";
+if (alien_1_color == "green") {
+    console.log("First Version : You just earned 5 points for shooting the alien");
+}
+else {
+    console.log("First Version : You just earned 10 points");
+}
+// This Version runs the Else Block
+var alien_2_color = "blue";
+if (alien_2_color == "green") {
+    console.log("Second Version : You just earned 5 points for shooting the alien");
+}
+else {
+    console.log("Second Version : You just earned 10 points");
+}
+// Question 27
+console.log("\n\tQuestion 28 Starts Here\t\n");
+// This Version runs for green alien
+var alien_one_color = "green";
+var alien_two_color = "yellow";
+var alien_3_color = "red";
+if (alien_one_color == "green") {
+    console.log("First Version : You just earned 5 points");
+}
+else if (alien_one_color == "yellow") {
+    console.log("First Version : You just earned 10 points");
+}
+else if (alien_one_color == "red") {
+    console.log("First Version : You just earned 15 points");
+}
+// This Version runs for yellow alien
+if (alien_two_color == "green") {
+    console.log("Second Version : You just earned 5 points");
+}
+else if (alien_two_color == "yellow") {
+    console.log("Second Version : You just earned 10 points");
+}
+else if (alien_two_color == "red") {
+    console.log("Second Version : You just earned 15 points");
+}
+// This Version runs for red alien
+if (alien_3_color == "green") {
+    console.log("Third Version : You just earned 5 points");
+}
+else if (alien_3_color == "yellow") {
+    console.log("Third Version : You just earned 10 points");
+}
+else if (alien_3_color == "red") {
+    console.log("Third Version : You just earned 15 points");
+}
---
+++ day09/main.ts
@@ -0,0 +1,55 @@
+// Question 25
+console.log("\n\tQuestion 25 Starts Here\t\n");
+let alien_color = "green";
+if (alien_color == "green") {
+  console.log("Player Earned 5 Points");
+} else {
+  console.log("No Output");
+}
+
+// Question 26
+console.log("\n\tQuestion 26 Starts Here\t\n");
+// This Version runs the If Block
+let alien_1_color = "green";
+if (alien_1_color == "green") {
+  console.log("First Version : You just earned 5 points for shooting the alien");
+} else {
+  console.log("First Version : You just earned 10 points");
+}
+// This Version runs the Else Block
+let alien_2_color = "blue";
+if (alien_2_color == "green") {
+  console.log("Second Version : You just earned 5 points for shooting the alien");
+} else {
+  console.log("Second Version : You just earned 10 points");
+}
+
+// Question 27
+console.log("\n\tQuestion 28 Starts Here\t\n");
+// This Version runs for green alien
+let alien_one_color = "green";
+let alien_two_color = "yellow";
+let alien_3_color = "red";
+if (alien_one_color == "green") {
+  console.log("First Version : You just earned 5 points");
+} else if (alien_one_color == "yellow") {
+  console.log("First Version : You just earned 10 points");
+} else if (alien_one_color == "red") {
+  console.log("First Version : You just earned 15 points");
+}
+// This Version runs for yellow alien
+if (alien_two_color == "green") {
+  console.log("Second Version : You just earned 5 points");
+} else if (alien_two_color == "yellow") {
+  console.log("Second Version : You just earned 10 points");
+} else if (alien_two_color == "red") {
+  console.log("Second Version : You just earned 15 points");
+}
+// This Version runs for red alien
+if (alien_3_color == "green") {
+  console.log("Third Version : You just earned 5 points");
+} else if (alien_3_color == "yellow") {
+  console.log("Third Version : You just earned 10 points");
+} else if (alien_3_color == "red") {
+  console.log("Third Version : You just earned 15 points");
+}
