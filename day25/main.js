f71e7e875bce0bc79fd4617807e3654448ed1a58
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-05-04 23:20:26 +0500

    Commited Day 25

---
+++ day25/main.js
@@ -0,0 +1,43 @@
+// Question 73
+console.log("\n\tQuestion 73 Starts Here\t\n");
+/*this code defines a function updateVariable that demonstrates variable
+modification within the function's scope. It creates a number variable
+with the value 20, logs it, updates the value to 52, and then logs it*/
+function updateVariable() {
+    var number = 20;
+    console.log("Initial value:", number);
+    number = 52;
+    console.log("Updated value:", number);
+}
+updateVariable();
+// Question 74
+console.log("\n\tQuestion 74 Starts Here\t\n");
+/*This code defines a function swapValues that utilizes a temporary
+variable to swap the values of two variables (a and b) and logs the
+values before and after the swap.*/
+function swapValues() {
+    var a = 15, b = 11;
+    console.log("Before swap: a =", a, "b =", b);
+    var temp = a;
+    a = b;
+    b = temp;
+    console.log("After swap: a =", a, "b =", b);
+}
+swapValues();
+// Question 75
+console.log("\n\tQuestion 75 Starts Here\t\n");
+/*This code demonstrates compound assignment operators performing
+operations and assignments on x in a single step and logging the results.*/
+function useCompoundOperators() {
+    var x = 5;
+    console.log("Initial x:", x);
+    x += 3;
+    console.log("After addition:", x);
+    x -= 2;
+    console.log("After subtraction:", x);
+    x *= 3;
+    console.log("After multiplication:", x);
+    x /= 2;
+    console.log("After division:", x);
+}
+useCompoundOperators();
