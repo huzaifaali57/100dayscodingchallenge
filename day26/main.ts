71b7484ca8eba75d050e9ebde5a675c393364bff
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-05-08 16:35:49 +0500

    Commited Day 26

--- day26/main.ts
+++ day26/main.ts
@@ -1,7 +1,7 @@
 // Question 76
 console.log("\n\tQuestion 76 Starts Here\t\n");
-/*The above code defines a function addNumbers that takes two 
-numbers as input, adds them together, and prints the sum.*/
+/*The above code defines a function addNumbers that takes two numbers as input, 
+adds them together, and prints the sum.*/
 function addNumbers(num1: number, num2: number): number {
     // Calculates the sum of num1 and num2
     return num1 + num2;
@@ -10,9 +10,8 @@ console.log(addNumbers(11, 219));
 
 // Question 77
 console.log("\n\tQuestion 77 Starts Here\t\n");
-/*Absolutely, this code defines a function called 
-greetUser that welcomes a user by name, using "anonymous" 
-as a default if no name is provided.*/
+/*Absolutely, this code defines a function called greetUser that welcomes a user 
+by name, using "anonymous" as a default if no name is provided.*/
 function greetUser(name: string = "anonymous user") {
     console.log(`Hello, ${name}!`);
 }
@@ -21,9 +20,8 @@ greetUser();
 
 // Question 78
 console.log("\n\tQuestion 78 Starts Here\t\n");
-/*This code defines two square functions, one declared 
-(squareDeclaration) and another as an expression (squareExpression), 
-both returning the square of a number.*/
+/*This code defines two square functions, one declared (squareDeclaration) and another 
+as an expression (squareExpression), both returning the square of a number.*/
 function squareDeclaration(number: number): number {
     return number * number;
 }
