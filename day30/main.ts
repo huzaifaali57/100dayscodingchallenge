aea81ba31880e6e81790da1eb1fb4deb426bb995
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-05-10 13:35:13 +0500

    Commited Day 30

---
+++ day30/main.ts
@@ -0,0 +1,29 @@
+// Question 88
+console.log("\n\tQuestion 88 Starts Here\t\n");
+/*This code defines a function to round a number to the 
+nearest integer and prints the results for 4.2 and 2.9.*/
+function roundToNearestInteger(num: number): number {
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(4.2));
+console.log(roundToNearestInteger(2.9));
+
+// Question 89
+console.log("\n\tQuestion 89 Starts Here\t\n");
+/*This code defines a function to convert a string to a number 
+(including decimals with parseFloat) and prints the conversions 
+of "100.29" and "45".*/
+function convertStringToNumber(str: string): number {
+    return parseFloat(str);
+}
+console.log(convertStringToNumber("100.29"));
+console.log(convertStringToNumber("45"));
+
+// Question 90
+console.log("\n\tQuestion 90 Starts Here\t\n");
+/*This code checks if a value is Not a Number (NaN).*/
+function isValueNaN(value: any): boolean {
+    return isNaN(value);
+}
+console.log(isValueNaN("what"));
+console.log(isValueNaN(456));
