ad54a5fdaec3f6fcb55c64818173e0078d152946
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-05-10 13:09:26 +0500

    Commited Day 28

---
+++ day28/main.ts
@@ -0,0 +1,28 @@
+// Question 82
+console.log("\n\tQuestion 82 Starts Here\t\n");
+/*The above code defines a function that returns the length of 
+a string and then prints the length of the string "Huzaifa".*/
+function stringLength(str: string): number {
+    return str.length;
+}
+console.log(stringLength("Huzaifa"));
+
+// Question 83
+console.log("\n\tQuestion 83 Starts Here\t\n");
+/*This code converts a string to uppercase and lowercase, 
+printing both versions.*/
+function convertCase(str: string) {
+    let upperStr = str.toUpperCase();
+    let lowerStr = str.toLowerCase();
+    console.log("Uppercase:", upperStr, "Lowercase:", lowerStr);
+}
+convertCase("Who are you?");
+
+// Question 84
+console.log("\n\tQuestion 84 Starts Here\t\n");
+/*This code defines a function to  rewrite "JavaScript" to 
+"TypeScript" in a sentence and prints the result.*/
+function replaceJavaScriptWithTypeScript(sentence: string): string {
+    return sentence.replace(/JavaScript/g, "TypeScript");
+}
+console.log(replaceJavaScriptWithTypeScript("I love JavaScript and JavaScript is awesome!"));
