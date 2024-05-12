90a6ecffcd66e0364b1be07b7762c4b85b0f106f
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-05-12 12:45:16 +0500

    Commited Day 34

---
+++ day34/main.js
@@ -0,0 +1,20 @@
+// Question 100
+console.log("\n\tQuestion 100 Starts Here\t\n");
+/*This line of code declares a constant named squareRoot and
+assigns it the square root of 144, then prints the result.*/
+var squareRoot = Math.sqrt(144);
+console.log(squareRoot);
+// Question 101
+console.log("\n\tQuestion 101 Starts Here\t\n");
+/*This code defines a function generating a random integer between
+1 and 10 (inclusive) and then calls it to print a random result.*/
+function getRandomInt() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(getRandomInt());
+// Question 102
+console.log("\n\tQuestion 102 Starts Here\t\n");
+/*This code calculates the absolute value of the difference between -5
+and -5 (which is 0) and stores it in a constant variable named difference.*/
+var difference = Math.abs(-5 - 5);
+console.log(difference);
