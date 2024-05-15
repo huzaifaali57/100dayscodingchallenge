92b709715a00841458b53a04edf696666d6d2afd
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-05-15 17:18:07 +0500

    Commited Day 40

---
+++ day40/main.js
@@ -0,0 +1,30 @@
+// Question 118
+console.log("\n\tQuestion 118 Starts Here\t\n");
+/*The code is a 'for' loop that prints numbers from 1 to 10
+(inclusive) to the console.*/
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+// Question 119
+console.log("\n\tQuestion 119 Starts Here\t\n");
+/*This code initializes a counter, then creates a loop
+that prints "Hello, World!" five times.*/
+var count = 0;
+while (count < 5) {
+    console.log("Hello, World!");
+    count++;
+}
+// Question 120
+console.log("\n\tQuestion 120 Starts Here\t\n");
+/*This code creates a for loop that iterates over favoriteMovies
+array and prints each movie title to the console.*/
+var favoriteMovies = [
+    "GodFather",
+    "Mission Impossible",
+    "John Wick",
+    "Golmaal",
+];
+for (var _i = 0, favoriteMovies_1 = favoriteMovies; _i < favoriteMovies_1.length; _i++) {
+    var movie = favoriteMovies_1[_i];
+    console.log(movie);
+}
---
+++ day40/main.ts
@@ -0,0 +1,31 @@
+// Question 118
+console.log("\n\tQuestion 118 Starts Here\t\n");
+/*The code is a 'for' loop that prints numbers from 1 to 10 
+(inclusive) to the console.*/
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Question 119
+console.log("\n\tQuestion 119 Starts Here\t\n");
+/*This code initializes a counter, then creates a loop 
+that prints "Hello, World!" five times.*/
+let count: number = 0;
+while (count < 5) {
+  console.log("Hello, World!");
+  count++;
+}
+
+// Question 120
+console.log("\n\tQuestion 120 Starts Here\t\n");
+/*This code creates a for loop that iterates over favoriteMovies 
+array and prints each movie title to the console.*/
+const favoriteMovies: string[] = [
+  "GodFather",
+  "Mission Impossible",
+  "John Wick",
+  "Golmaal",
+];
+for (const movie of favoriteMovies) {
+  console.log(movie);
+}
