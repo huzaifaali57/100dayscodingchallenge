72958a44c5d0ec4e1ddb21ea49386651769eb7de
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-06-02 15:14:23 +0500

    Commited Day 51

---
+++ day51/main.js
@@ -0,0 +1,8 @@
+// Day 51
+var lesson = "I have read the Article provided.";
+var learning1 = "Solve problems and also practice on GeeksforGeeks, HackerRank, and Codecademy sites etc.";
+var learning2 = "Solve the MCQs Questions in Programming.";
+var learning3 = "Join a community where there are experts and take help from them.";
+var learning4 = "Start with smaller problems to complex problem.";
+console.log("\t\n" + lesson + "\n\t");
+console.log(learning1 + "\n" + learning2 + "\n" + learning3 + "\n" + learning4);
---
+++ day51/main.ts
@@ -0,0 +1,8 @@
+// Day 51
+let lesson = "I have read the Article provided."
+let learning1 = "Solve problems and also practice on GeeksforGeeks, HackerRank, and Codecademy sites etc."
+let learning2 = "Solve the MCQs Questions in Programming."
+let learning3 = "Join a community where there are experts and take help from them."
+let learning4 = "Start with smaller problems to complex problem."
+console.log("\t\n" + lesson + "\n\t")
+console.log(learning1 + "\n" + learning2 + "\n" + learning3 + "\n" + learning4)
