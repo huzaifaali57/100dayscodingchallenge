cf25ea0e8a87a1afa7225149d99e2e1f63267d66
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-06-04 15:02:52 +0500

    Commited Day 52

---
+++ day52/main.ts
@@ -0,0 +1,9 @@
+// Day 52
+let lesson = "I have read the Article provided for Day 52."
+let learning = "The document talks about JavaScript, a programming language that can be " +
+               "\nused to make webpages interactive. It explains what JavaScript is and " +
+               "\nhow it works in web browsers. It also discusses what JavaScript can and " +
+               "\ncan't do for security reasons, and mentions other languages that can be " + 
+               "\nconverted to JavaScript to give programmers more options."
+console.log("\t\n" + lesson + "\n\t")
+console.log(learning)
