0a7c4d8d076e7db89ac26475709f321c77a39464
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-06-04 16:17:32 +0500

    Commited Day 53

---
+++ day53/main.ts
@@ -0,0 +1,4 @@
+// Day 53
+const day = "Day 53 Task"
+let lesson = "In Day 53 I got an overview on Chrome Dev Tools for example \n1)how to debug your code. \n2)How to edit code while running and on hand changes. \n3)You can view your website on different size of devices and much more."
+console.log(`\n\t ${day} \t\n \n${lesson}`)
