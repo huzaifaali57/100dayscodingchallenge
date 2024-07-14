6c922bddacef4927be35994e8c70d0ad1a39bbae
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-07-14 20:54:24 +0500

    Commited Day 55

---
+++ day55/main.js
@@ -0,0 +1,11 @@
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+
+let Name = prompt('What is your name?', );
+
+alert(`Your name is ${Name}`);
