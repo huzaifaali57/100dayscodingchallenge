68c7420018a9bcece53fa7c92fbe00887e2b372b
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-07-15 23:08:55 +0500

    Commited Day 57

---
+++ day57/script.js
@@ -0,0 +1,19 @@
+if ("0") {
+    alert( 'Hello' );
+  }
+
+let value = prompt('What is the "official" name of JavaScript?','')
+if(value == 'ECMAScript'){
+    alert('Correct!')
+}else{
+    alert('You dont know ECMAScript.')
+}
+
+let value2 = prompt('Enter a number ?', 0)
+if(value2 > 0){
+    alert(1)
+}else if(value2 < 0){
+    alert(-1)
+}else if(value2 == 0){
+    alert(0)
+}
