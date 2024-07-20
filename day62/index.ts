a8cb70287c489d84948c12348bc6abc20a1fdd59
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-07-20 16:19:50 +0500

    Commited Day 62

---
+++ day62/index.ts
@@ -0,0 +1,4 @@
+// Day 62 was all about installing the and setting up the 
+// typescript environment in the IDE i.e. VS Code
+// the next task was to run the code below
+console.log(Math.floor(11.3));
