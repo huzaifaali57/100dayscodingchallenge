466020ad3eb51fe8293a9b614a3e4bcf760ee06d
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-07-21 18:54:11 +0500

    Commited Day 63

---
+++ day63/main.js
@@ -0,0 +1,18 @@
+// Task as per Day 63
+
+// Type Annotation
+var numberIs = 10;
+var stringIs = "string";
+var booleanIs = true;
+console.log(numberIs, stringIs, booleanIs);
+
+// Any Type
+var anyIs;
+anyIs = 20;
+anyIs = "any";
+anyIs = true;
+console.log(anyIs);
+
+// Dynamic Type Determination
+var textIs = "How are you?";
+console.log(textIs);
---
+++ day63/main.ts
@@ -0,0 +1,20 @@
+// Task as per Day 63
+
+// Type Annotation
+let numberIs : number = 10;
+let stringIs : string = "string";
+let booleanIs : boolean = true;
+console.log(numberIs, stringIs, booleanIs)
+
+
+// Any Type
+let anyIs : any;
+anyIs = 20;
+anyIs = "any";
+anyIs = true;
+console.log(anyIs)
+
+
+// Dynamic Type Determination
+let textIs = "How are you?";
+console.log(textIs)
