b0bb4953ed44cdc87ce02ca5069376127954af10
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-07-23 17:38:51 +0500

    Commited Day 64

---
+++ day64/main.js
@@ -0,0 +1,10 @@
+//  Day 64
+// Type Annotations With Objects
+var personOneOrder;
+personOneOrder = {
+    tableNo: 101,
+    food: "Chicken Wings",
+    coldDrink: "no israeli product",
+    totalPrice: 120
+};
+console.log(personOneOrder);
---
+++ day64/main.ts
@@ -0,0 +1,18 @@
+//  Day 64
+// Type Annotations With Objects
+
+let personOneOrder : {
+    tableNo : number;
+    food : string;
+    coldDrink? : string;
+    totalPrice : number;
+}
+
+personOneOrder = {
+    tableNo : 101,
+    food : "Chicken Wings",
+    coldDrink : "no israeli product",
+    totalPrice : 120
+}
+
+console.log(personOneOrder)
